f379d287d877c19c73da558b70322ef7230b437c
samuelrah <samuel.rahseparmohammadi@elev.ga.ntig.se>
2025-01-24 14:11:40 +0100

    hello

--- textäventyr_draft.py
+++ textäventyr_draft.py
@@ -1,19 +1,34 @@
-meny = ("1")
+import os
+os.system
 
+# Define the menu options
+menu = ("1", "2", "3")
+
+# Start the menu loop
 while True:
-    print("Meny")
-    print("1- starta spelet")
+    print("\nMENY\n")
+    print("1- Starta spelet")
+    print("2- Regler")
+    print("3- Credits")
 
-    meny_input = input("skriv ett nummer:")
+    # Prompt user input
+    user_input = input("Skriv numret av ett av valen: ")
 
-    if meny_input in meny:
-        meny_input == "1"
-        print("starta spel.")
-
+    # Check if the input is valid
+    if user_input in menu:
+        if user_input == "1":
+            break  # Exit the menu loop
+        elif user_input == "2":
+            print("\nRegler: Följ instruktionerna för att spela spelet.")
+        elif user_input == "3":
+            print("\nCredits: Samuel Rahsepar.")
     else:
-        print ("ogiltigt vall försök igen.")
+        print("Ogiltigt val! Försök igen.")
 
 
 
-def rum_1():
-    print ("Du har ")
+
+
+print("Du är fast i en fängelsecell och måste fly från fängelset. Du måste vara försiktig för att inte bli upptäckt av vakterna.")
+print("Du kollar runt i cellen, du det en nyckel på golvet och fälgelse dörren är olåst.")
+
